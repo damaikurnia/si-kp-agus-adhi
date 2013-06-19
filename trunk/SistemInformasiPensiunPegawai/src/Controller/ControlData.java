ee87a6eb9f3aff6d7e0e5219ffc857fba900551d
agustinusakava <agustinusakava@274fad37-ebee-8a79-adcd-6f23909e3f15>
2013-06-19 03:46:34 +0000

    login

--- trunk/SistemInformasiPensiunPegawai/src/Controller/ControlData.java
+++ trunk/SistemInformasiPensiunPegawai/src/Controller/ControlData.java
@@ -2,19 +2,20 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller;
 
 import java.sql.Connection;
-
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author a9uszT
  */
 public class ControlData {
-private Connection conn;
+
+    private Connection conn;
 
     public ControlData(Connection conn) {
         this.conn = conn;
@@ -25,12 +26,26 @@ private Connection conn;
         return control;
     }
 
-    public boolean Login(String id, String passwd) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public boolean Login(String id, String password) throws SQLException {
+        PreparedStatement stmt = null;
+        boolean cek = false;
+        ResultSet result = null;
+        conn.setAutoCommit(false);
+        String query = "SELECT idPegawai,passwordpegawai FROM operator "
+                + "where idPegawai=? AND passwordpegawai=? ";
+        stmt = conn.prepareStatement(query);
+        stmt.setString(1, id);
+        stmt.setString(2, password);
+        result = stmt.executeQuery();
+        if (result.next()) {
+            cek = true;
+        }
+        conn.commit();
+        return cek;
+
     }
 
     public String cariPegawai(String id) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
-    
 }
