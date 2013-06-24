cb07f66a70948308f97fe9ec63323a52773be04a
agustinusakava <agustinusakava@274fad37-ebee-8a79-adcd-6f23909e3f15>
2013-06-24 13:22:19 +0000

    Model

---
+++ trunk/SistemInformasiPensiunPegawai/src/Model/SK_CPNS.java
@@ -0,0 +1,212 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author a9uszT
+ */
+public class SK_CPNS {
+    private String id_SuratCPNS;
+    private String tempat_surat;
+    private String tanggal_surat;
+    private String nama_pemilik;
+    private String tempat_lahir;
+    private String tanggal_lahir;
+    private String partikelir;
+    private String tmt_partikelir;
+    private String Profesi;
+    private String Sekolah;
+    private String nip_lama;
+    private String nip_baru;
+
+    public SK_CPNS() {
+    }
+
+    public SK_CPNS(String id_SuratCPNS, String tempat_surat, String tanggal_surat, String nama_pemilik, String tempat_lahir, String tanggal_lahir, String partikelir, String tmt_partikelir, String Profesi, String Sekolah, String nip_lama, String nip_baru) {
+        this.id_SuratCPNS = id_SuratCPNS;
+        this.tempat_surat = tempat_surat;
+        this.tanggal_surat = tanggal_surat;
+        this.nama_pemilik = nama_pemilik;
+        this.tempat_lahir = tempat_lahir;
+        this.tanggal_lahir = tanggal_lahir;
+        this.partikelir = partikelir;
+        this.tmt_partikelir = tmt_partikelir;
+        this.Profesi = Profesi;
+        this.Sekolah = Sekolah;
+        this.nip_lama = nip_lama;
+        this.nip_baru = nip_baru;
+    }
+
+    /**
+     * @return the id_SuratCPNS
+     */
+    public String getId_SuratCPNS() {
+        return id_SuratCPNS;
+    }
+
+    /**
+     * @param id_SuratCPNS the id_SuratCPNS to set
+     */
+    public void setId_SuratCPNS(String id_SuratCPNS) {
+        this.id_SuratCPNS = id_SuratCPNS;
+    }
+
+    /**
+     * @return the tempat_surat
+     */
+    public String getTempat_surat() {
+        return tempat_surat;
+    }
+
+    /**
+     * @param tempat_surat the tempat_surat to set
+     */
+    public void setTempat_surat(String tempat_surat) {
+        this.tempat_surat = tempat_surat;
+    }
+
+    /**
+     * @return the tanggal_surat
+     */
+    public String getTanggal_surat() {
+        return tanggal_surat;
+    }
+
+    /**
+     * @param tanggal_surat the tanggal_surat to set
+     */
+    public void setTanggal_surat(String tanggal_surat) {
+        this.tanggal_surat = tanggal_surat;
+    }
+
+    /**
+     * @return the nama_pemilik
+     */
+    public String getNama_pemilik() {
+        return nama_pemilik;
+    }
+
+    /**
+     * @param nama_pemilik the nama_pemilik to set
+     */
+    public void setNama_pemilik(String nama_pemilik) {
+        this.nama_pemilik = nama_pemilik;
+    }
+
+    /**
+     * @return the tempat_lahir
+     */
+    public String getTempat_lahir() {
+        return tempat_lahir;
+    }
+
+    /**
+     * @param tempat_lahir the tempat_lahir to set
+     */
+    public void setTempat_lahir(String tempat_lahir) {
+        this.tempat_lahir = tempat_lahir;
+    }
+
+    /**
+     * @return the tanggal_lahir
+     */
+    public String getTanggal_lahir() {
+        return tanggal_lahir;
+    }
+
+    /**
+     * @param tanggal_lahir the tanggal_lahir to set
+     */
+    public void setTanggal_lahir(String tanggal_lahir) {
+        this.tanggal_lahir = tanggal_lahir;
+    }
+
+    /**
+     * @return the partikelir
+     */
+    public String getPartikelir() {
+        return partikelir;
+    }
+
+    /**
+     * @param partikelir the partikelir to set
+     */
+    public void setPartikelir(String partikelir) {
+        this.partikelir = partikelir;
+    }
+
+    /**
+     * @return the tmt_partikelir
+     */
+    public String getTmt_partikelir() {
+        return tmt_partikelir;
+    }
+
+    /**
+     * @param tmt_partikelir the tmt_partikelir to set
+     */
+    public void setTmt_partikelir(String tmt_partikelir) {
+        this.tmt_partikelir = tmt_partikelir;
+    }
+
+    /**
+     * @return the Profesi
+     */
+    public String getProfesi() {
+        return Profesi;
+    }
+
+    /**
+     * @param Profesi the Profesi to set
+     */
+    public void setProfesi(String Profesi) {
+        this.Profesi = Profesi;
+    }
+
+    /**
+     * @return the Sekolah
+     */
+    public String getSekolah() {
+        return Sekolah;
+    }
+
+    /**
+     * @param Sekolah the Sekolah to set
+     */
+    public void setSekolah(String Sekolah) {
+        this.Sekolah = Sekolah;
+    }
+
+    /**
+     * @return the nip_lama
+     */
+    public String getNip_lama() {
+        return nip_lama;
+    }
+
+    /**
+     * @param nip_lama the nip_lama to set
+     */
+    public void setNip_lama(String nip_lama) {
+        this.nip_lama = nip_lama;
+    }
+
+    /**
+     * @return the nip_baru
+     */
+    public String getNip_baru() {
+        return nip_baru;
+    }
+
+    /**
+     * @param nip_baru the nip_baru to set
+     */
+    public void setNip_baru(String nip_baru) {
+        this.nip_baru = nip_baru;
+    }
+
+    
+}
