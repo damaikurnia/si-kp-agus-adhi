de8cd2df278f7b99405a4e808a2531c571a9c69a
black.paradekid <black.paradekid@274fad37-ebee-8a79-adcd-6f23909e3f15>
2013-06-26 01:55:29 +0000

    tambah

---
+++ SistemInformasiPensiunPegawai/src/Model/SPTKG_Terakhir.java
@@ -0,0 +1,195 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Mich
+ */
+public class SPTKG_Terakhir {
+    private String id_SuratSPTKGTerakhir;
+    private String tempat_surat;
+    private String tanggal_surat;
+    private String nama_pemilik;
+    private String nip_baru;
+    private String pangkat_baru;
+    private String golongan_ruang_baru;
+    private String Sekolah;
+    private String gajipokok_lama;
+    private String gajipokok_baru;
+    private String tmt_baru;
+    
+    public SPTKG_Terakhir(){}
+    
+    public SPTKG_Terakhir(String id,String tmpt,String tgl,String nama,
+            String nip,String pangkat,String gol,String sekolah,String gaji_lama,
+            String gaji_baru,String tmt){
+        this.id_SuratSPTKGTerakhir = id;
+        this.tempat_surat = tmpt;
+        this.tanggal_surat = tgl;
+        this.nama_pemilik = nama;
+        this.nip_baru = nip;
+        this.pangkat_baru = pangkat;
+        this.golongan_ruang_baru = gol;
+        this.Sekolah = sekolah;
+        this.gajipokok_lama = gaji_lama;
+        this.gajipokok_baru = gaji_baru;
+        this.tmt_baru = tmt;
+    }
+    
+    /**
+     * @return the id_SuratSPTKGTerakhir
+     */
+    public String getId_SuratSPTKGTerakhir() {
+        return id_SuratSPTKGTerakhir;
+    }
+
+    /**
+     * @param id_SuratSPTKGTerakhir the id_SuratSPTKGTerakhir to set
+     */
+    public void setId_SuratSPTKGTerakhir(String id_SuratSPTKGTerakhir) {
+        this.id_SuratSPTKGTerakhir = id_SuratSPTKGTerakhir;
+    }
+
+    /**
+     * @return the tempat_surat
+     */
+    public String getTempat_surat() {
+        return tempat_surat;
+    }
+
+    /**
+     * @param tempat_surat the tempat_surat to set
+     */
+    public void setTempat_surat(String tempat_surat) {
+        this.tempat_surat = tempat_surat;
+    }
+
+    /**
+     * @return the tanggal_surat
+     */
+    public String getTanggal_surat() {
+        return tanggal_surat;
+    }
+
+    /**
+     * @param tanggal_surat the tanggal_surat to set
+     */
+    public void setTanggal_surat(String tanggal_surat) {
+        this.tanggal_surat = tanggal_surat;
+    }
+
+    /**
+     * @return the nama_pemilik
+     */
+    public String getNama_pemilik() {
+        return nama_pemilik;
+    }
+
+    /**
+     * @param nama_pemilik the nama_pemilik to set
+     */
+    public void setNama_pemilik(String nama_pemilik) {
+        this.nama_pemilik = nama_pemilik;
+    }
+
+    /**
+     * @return the nip_baru
+     */
+    public String getNip_baru() {
+        return nip_baru;
+    }
+
+    /**
+     * @param nip_baru the nip_baru to set
+     */
+    public void setNip_baru(String nip_baru) {
+        this.nip_baru = nip_baru;
+    }
+
+    /**
+     * @return the pangkat_baru
+     */
+    public String getPangkat_baru() {
+        return pangkat_baru;
+    }
+
+    /**
+     * @param pangkat_baru the pangkat_baru to set
+     */
+    public void setPangkat_baru(String pangkat_baru) {
+        this.pangkat_baru = pangkat_baru;
+    }
+
+    /**
+     * @return the golongan_ruang_baru
+     */
+    public String getGolongan_ruang_baru() {
+        return golongan_ruang_baru;
+    }
+
+    /**
+     * @param golongan_ruang_baru the golongan_ruang_baru to set
+     */
+    public void setGolongan_ruang_baru(String golongan_ruang_baru) {
+        this.golongan_ruang_baru = golongan_ruang_baru;
+    }
+
+    /**
+     * @return the Sekolah
+     */
+    public String getSekolah() {
+        return Sekolah;
+    }
+
+    /**
+     * @param Sekolah the Sekolah to set
+     */
+    public void setSekolah(String Sekolah) {
+        this.Sekolah = Sekolah;
+    }
+
+    /**
+     * @return the gajipokok_lama
+     */
+    public String getGajipokok_lama() {
+        return gajipokok_lama;
+    }
+
+    /**
+     * @param gajipokok_lama the gajipokok_lama to set
+     */
+    public void setGajipokok_lama(String gajipokok_lama) {
+        this.gajipokok_lama = gajipokok_lama;
+    }
+
+    /**
+     * @return the gajipokok_baru
+     */
+    public String getGajipokok_baru() {
+        return gajipokok_baru;
+    }
+
+    /**
+     * @param gajipokok_baru the gajipokok_baru to set
+     */
+    public void setGajipokok_baru(String gajipokok_baru) {
+        this.gajipokok_baru = gajipokok_baru;
+    }
+
+    /**
+     * @return the tmt_baru
+     */
+    public String getTmt_baru() {
+        return tmt_baru;
+    }
+
+    /**
+     * @param tmt_baru the tmt_baru to set
+     */
+    public void setTmt_baru(String tmt_baru) {
+        this.tmt_baru = tmt_baru;
+    }
+}
