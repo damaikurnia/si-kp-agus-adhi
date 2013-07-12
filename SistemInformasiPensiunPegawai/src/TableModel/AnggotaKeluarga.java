3e10c0ac3c10c9c8cb7b4e4dc1ae38191d941be9
black.paradekid <black.paradekid@274fad37-ebee-8a79-adcd-6f23909e3f15>
2013-07-12 01:31:18 +0000

    tes komit

---
+++ SistemInformasiPensiunPegawai/src/TableModel/AnggotaKeluarga.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TableModel;
+
+/**
+ *
+ * @author Mich
+ */
+public class AnggotaKeluarga {
+    
+}
--- SistemInformasiPensiunPegawai/src/View/TambahPegawai.java
+++ SistemInformasiPensiunPegawai/src/View/TambahPegawai.java
@@ -505,6 +505,7 @@ public class TambahPegawai extends javax.swing.JFrame {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/PIcture/header2.jpg"))); // NOI18N
         jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
